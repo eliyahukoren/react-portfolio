79d1d5d8381d41c7d0f559c56d4e7bc1ff203a2d
eliyahukoren <elik.koren@gmail.com>
2022-12-04 13:15:03 +0200

    a bit about props abd state

--- src/props/GuestList.tsx
+++ src/props/GuestList.tsx
@@ -1,7 +1,15 @@
 import { useState} from "react";
 
 const GuestList: React.FC = () => {
-	const [name, setName ] = useState('');
+	const [name, setName ] = useState<string>('');
+	const [guests, setGuests ] = useState<string[]>([]);
+
+	const onClick = () => {
+		if(name.trim() !== ''){
+			setName('');
+			setGuests([...guests, name]);
+		}
+	}
 
 	return (
 		<div className="row justify-content-center my-auto mt-5 mw-50">
@@ -9,8 +17,20 @@ const GuestList: React.FC = () => {
 				<div className="card shadow">
 					<div className="card-body">
 						<h5 className="card-title">Guest List</h5>
-						<input type="text" className="form-control mb-2" value={name} onChange={(e) => setName(e.target.value)} />
-						<button className="btn btn-primary">
+
+						<ul className="list-group list-group-flush">
+							{guests.map((g, index) => (
+								<li key={index} className="list-group-item">{g}</li>
+							))}
+						</ul>
+
+						<input
+							type="text"
+							className="form-control mb-2"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+						<button className="btn btn-primary" onClick={onClick}>
 							Add Guest
 						</button>
 					</div>

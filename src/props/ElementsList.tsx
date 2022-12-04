d3f4bd95bf511b368cf7a366daaa4c60392bcedf
eliyahukoren <elik.koren@gmail.com>
2022-12-04 16:10:58 +0200

    switch to components

---
+++ src/props/ElementsList.tsx
@@ -0,0 +1,15 @@
+interface IElementsList {
+	elements: string[];
+}
+export const ElementsList: React.FC<IElementsList> = (props: IElementsList) => {
+	return (
+		<ul className="list-group list-group-flush">
+			{props.elements.map((element, index) => (
+				<li key={index} className="list-group-item">
+					{element}
+					{element}
+				</li>
+			))}
+		</ul>
+	);
+};
